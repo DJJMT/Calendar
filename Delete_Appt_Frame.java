95e772b8f6cb52b9742a3746a6170885fb8733bd
DJJMT <dkordsme@uark.edu>
2014-04-18 10:52:54 -0500

    Create Delete_Appt_Frame.java
    Frame to delete appointments..just the skeleton right now but can work on it later.
    --Taylor

---
+++ Delete_Appt_Frame.java
@@ -0,0 +1,22 @@
+//Appointment Deletion Frame
+import java.awt.*;
+import javax.swing.*;
+
+import java.awt.event.*;
+
+public class Delete_Appt_Frame extends JFrame implements ActionListener
+{
+        //initialises the frame and opens it
+        public Delete_Appt_Frame()
+        {
+                //JButton open = new JButton("New Appointment");
+                //open.addActionListener(this);
+                //add(open);
+                setVisible(true);
+        }
+
+        public void actionPerformed(ActionEvent event)
+        {
+                //code for the new frame
+        }
+}
